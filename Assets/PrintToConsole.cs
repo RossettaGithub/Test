17227a41a7bf2f5465c9b3277d04433ec7be7515
RossettaBitbucket <izzyshocks@gmail.com>
2021-12-17 17:35:45 +0000

    end

--- Assets/PrintToConsole.cs
+++ Assets/PrintToConsole.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class P : MonoBehaviour
+public class PrintToConsole : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+      Debug.Log("Hi");
     }
 
     // Update is called once per frame
